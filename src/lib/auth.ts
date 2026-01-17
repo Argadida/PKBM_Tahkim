6649e510b224956585533945d4ecc841ec15656b
Argadida <egakarga369@gmail.co>
2026-01-17 17:18:56 +0700

    nw3

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,10 +4,14 @@ import { db } from "./db";
 import * as schema from "../db/schema";
 
 export const auth = betterAuth({
+    secret: process.env.BETTER_AUTH_SECRET!,   // 🔴 WAJIB
+    baseURL: process.env.BETTER_AUTH_URL!,     // ✅ DISARANKAN
+
     database: drizzleAdapter(db, {
-        provider: "sqlite",
-        schema: schema,
+        provider: "sqlite", // ⬅️ tetap OK walau pakai Turso
+        schema,
     }),
+
     emailAndPassword: {
         enabled: true,
     },
