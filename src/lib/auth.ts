c1ff994e4a848b8793780112ca540e475d90f5ed
Argadida <egakarga369@gmail.co>
2026-01-19 10:57:52 +0700

    n2

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,12 +4,12 @@ import { db } from "./db";
 import * as schema from "../db/schema";
 
 export const auth = betterAuth({
-    secret: "kjsd8734nmsdf834jfd734nmsdf", // Sementara hardcode untuk fix error undefined
+    secret: process.env.BETTER_AUTH_SECRET,
     baseURL: process.env.BETTER_AUTH_URL || "https://pkbm-tahkim.vercel.app", // Fallback URL
 
 
     database: drizzleAdapter(db, {
-        provider: "sqlite", // ⬅️ tetap OK walau pakai Turso
+        provider: "pg",
         schema,
     }),
 
