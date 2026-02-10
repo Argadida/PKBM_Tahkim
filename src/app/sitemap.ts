1f821dae84454379f05cb12aeca455e8a198e366
Argadida <egakarga369@gmail.co>
2026-02-10 09:30:02 +0700

    tambah sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,10 +5,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
 
   return [
     { url: baseUrl, lastModified: new Date() },
-    { url: `${baseUrl}/program`, lastModified: new Date() },
+    { url: `${baseUrl}/programs`, lastModified: new Date() },
     { url: `${baseUrl}/keunggulan`, lastModified: new Date() },
     { url: `${baseUrl}/artikel`, lastModified: new Date() },
     { url: `${baseUrl}/kontak`, lastModified: new Date() },
     { url: `${baseUrl}/programs/paud`, lastModified: new Date() },
+    { url: `${baseUrl}/programs/paket-c`, lastModified: new Date() },
   ];
 }
